f1921ee3d68465c0c17ae8edc778941eca18edef
Patrice Allardin <patrice.allardin@free.fr>
2019-04-23 11:20:34 +0200

    tp1.2v3

--- src/main/java/fr/diginamic/entites/Client.java
+++ src/main/java/fr/diginamic/entites/Client.java
@@ -4,13 +4,12 @@ public class Client {
 	public int numeroCompte;
 	public String nom;
 	public String prenom;
-	
-	public Client (int param, String param2, String param3) {
+
+	public Client(int param, String param2, String param3) {
 		numeroCompte = param;
 		nom = param2;
 		prenom = param3;
 		System.out.println("Nouvelle instance créée : " + param);
-		//numeroCompte = param;
 		System.out.println("Numéro de compte : " + numeroCompte);
 		System.out.println("Nom : " + nom);
 		System.out.println("Prénom : " + prenom);
