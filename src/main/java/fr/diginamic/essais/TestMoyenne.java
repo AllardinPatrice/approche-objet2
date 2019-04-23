25f2f1ea11846028437c854153840fffd966a482
Patrice Allardin <patrice.allardin@free.fr>
2019-04-23 17:00:49 +0200

    tp3.2v1.2

---
+++ src/main/java/fr/diginamic/essais/TestMoyenne.java
@@ -0,0 +1,33 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.operations.CalculMoyenne;
+
+public class TestMoyenne {
+
+	public static void main(String[] args) {
+		CalculMoyenne c1 = new CalculMoyenne(2);
+		c1.ajout(1);
+		c1.ajout(2);
+		System.out.println("Moyenne : " + c1.calcul());
+
+		CalculMoyenne c2 = new CalculMoyenne(8);
+		c2.ajout(1);
+		c2.ajout(2);
+		c2.ajout(3);
+		c2.ajout(4);
+		c2.ajout(5);
+		c2.ajout(6);
+		c2.ajout(7);
+		c2.ajout(8);
+		System.out.println("Moyenne : " + c2.calcul());
+
+		CalculMoyenne c3 = new CalculMoyenne(-1);
+
+		CalculMoyenne c4 = new CalculMoyenne(2);
+		c4.ajout(1);
+		c4.ajout(2);
+		c4.ajout(3);
+		System.out.println("Moyenne : " + c4.calcul());
+	}
+
+}
--- src/main/java/fr/diginamic/operations/CalculMoyenne.java
+++ src/main/java/fr/diginamic/operations/CalculMoyenne.java
@@ -1,5 +1,33 @@
 package fr.diginamic.operations;
 
 public class CalculMoyenne {
-	public double[] tab;
+	public double[] tab = new double[0];
+	public int index = 0;
+	public int tailleTableau = 0;
+
+	public CalculMoyenne(int tailleTableau) {
+		if (tailleTableau > 0) {
+			this.tailleTableau = tailleTableau;
+			tab = new double[tailleTableau];
+		} else {
+			System.err.println("Taille de tableau négative");
+		}
+	}
+
+	public void ajout(double param) {
+		if (index < tailleTableau) {
+			tab[index++] = param;
+		} else {
+			System.err.println("Trop de valeurs");
+		}
+
+	}
+
+	public double calcul() {
+		double moyenne = 0.0;
+		for (int i = 0; i < tailleTableau; i++) {
+			moyenne += tab[i] / tailleTableau;
+		}
+		return moyenne;
+	}
 }
